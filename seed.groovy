baaee36216b3dbf5a6f1898d3bac3e781d39ecc1
arunraju359 <arunraju359@gmail.com>
2022-07-13 11:09:54 +0530

    Commmit

--- seed.groovy
+++ seed.groovy
@@ -1,3 +1,12 @@
 freeStyleJob('example') {}
-freeStyleJob('example1') {}
+freeStyleJob('example1') {
+
+}
 freeStyleJob('Sampledemp'){}
+freeStyleJob('example-Config') {
+    logRotator(-1, 10)
+    jdk('Java 8')
+   
+    
+    
+}
